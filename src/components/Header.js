22fe41020c70eee34783bb12d1c347520ea84a39
mail21 <ismailidn1@gmail.com>
2020-07-31 10:21:37 +0700

    setup and make header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./../image/logo.png";
+
+function Header() {
+  return (
+    <header className="center">
+      <img src={logo} alt="Logo Breaking bad" />
+    </header>
+  );
+}
+
+export default Header;
